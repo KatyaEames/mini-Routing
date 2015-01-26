712842f3eda5e2a54405309c227b2e4e2514cb95
KatyaEames <katya.eames@gmail.com>
2015-01-26 16:00:06 -0700

    h

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 var app = angular.module('miniRoute', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'js/home/homeTmpl.html',
+			controller: 'homeCtrl'
+		})
+		.when('/products/:id', {
+			templateUrl: 'js/products.productsTmpl.html',
+			controller: 'productsCtrl'
+		})
+		.when('settings', {
+			templateUrl: 'js/settings/settingTmpl.html',
+			controller: 'settingsCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		})
+});
